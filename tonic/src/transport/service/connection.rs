47ac1d2dc3d64cd70739f76f122cec8897d8f301
Alex Rudy <github@alexrudy.net>
2024-03-31 05:08:20 +0000

    Fix transport service
    Provides adaptors between the Hyper-1.0 stable traits and traits needed by tonic and axum to send and recieve requests as a client.

--- tonic/src/transport/service/connection.rs
+++ tonic/src/transport/service/connection.rs
@@ -1,8 +1,8 @@
 use super::{grpc_timeout::GrpcTimeout, reconnect::Reconnect, AddOrigin, UserAgent};
 use crate::transport::{BoxFuture, Endpoint};
 use http::Uri;
-use hyper::client::conn::http2::Builder;
 use hyper::rt;
+use hyper::{client::conn::http2::Builder, rt::Executor};
 use std::{
     fmt,
     task::{Context, Poll},
@@ -30,7 +30,7 @@ impl Connection {
         C::Future: Unpin + Send,
         C::Response: rt::Read + rt::Write + Unpin + Send + 'static,
     {
-        let mut settings = Builder::new(endpoint.executor)
+        let mut settings: Builder<super::SharedExec> = Builder::new(endpoint.executor)
             .initial_stream_window_size(endpoint.init_stream_window_size)
             .initial_connection_window_size(endpoint.init_connection_window_size)
             .keep_alive_interval(endpoint.http2_keep_alive_interval)
@@ -60,11 +60,12 @@ impl Connection {
             .option_layer(endpoint.rate_limit.map(|(l, d)| RateLimitLayer::new(l, d)))
             .into_inner();
 
-        let conn = Reconnect::new(connector, endpoint.uri.clone(), is_lazy);
-        let inner = stack.layer(conn);
+        let make_service = MakeSendRequestService::new(connector, endpoint, settings);
+
+        let conn = Reconnect::new(make_service, endpoint.uri.clone(), is_lazy);
 
         Self {
-            inner: BoxService::new(inner),
+            inner: BoxService::new(stack.layer(conn)),
         }
     }
 
@@ -114,5 +115,91 @@ impl Load for Connection {
 impl fmt::Debug for Connection {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Connection").finish()
+    }
+}
+
+struct SendRequest {
+    inner: hyper::client::conn::http2::SendRequest<axum::body::Body>,
+}
+
+impl From<hyper::client::conn::http2::SendRequest<axum::body::Body>> for SendRequest {
+    fn from(inner: hyper::client::conn::http2::SendRequest<axum::body::Body>) -> Self {
+        Self { inner }
+    }
+}
+
+impl tower::Service<http::Request<axum::body::Body>> for SendRequest {
+    type Response = http::Response<axum::body::Body>;
+    type Error = crate::Error;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx).map_err(Into::into)
+    }
+
+    fn call(&mut self, req: http::Request<axum::body::Body>) -> Self::Future {
+        let fut = self.inner.send_request(req);
+
+        Box::pin(async move {
+            fut.await
+                .map_err(Into::into)
+                .map(|res| res.map(|body| axum::body::Body::new(body)))
+        })
+    }
+}
+
+struct MakeSendRequestService<C> {
+    connector: C,
+    endpoint: Endpoint,
+    settings: Builder<super::SharedExec>,
+}
+
+impl<C> MakeSendRequestService<C> {
+    fn new(connector: C, endpoint: Endpoint, settings: Builder<super::SharedExec>) -> Self {
+        Self {
+            connector,
+            endpoint,
+            settings,
+        }
+    }
+}
+
+impl<C> tower::Service<Uri> for MakeSendRequestService<C>
+where
+    C: Service<Uri> + Send + 'static,
+    C::Error: Into<crate::Error> + Send,
+    C::Future: Unpin + Send,
+    C::Response: rt::Read + rt::Write + Unpin + Send + 'static,
+{
+    type Response = SendRequest;
+    type Error = crate::Error;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.connector.poll_ready(cx).map_err(Into::into)
+    }
+
+    fn call(&mut self, req: Uri) -> Self::Future {
+        let fut = self.connector.call(req);
+        Box::pin(async move {
+            let io = fut.await.map_err(Into::into)?;
+            let (send_request, conn) = Builder::new(self.endpoint.executor)
+                .initial_stream_window_size(self.endpoint.init_stream_window_size)
+                .initial_connection_window_size(self.endpoint.init_connection_window_size)
+                .keep_alive_interval(self.endpoint.http2_keep_alive_interval)
+                .handshake(io)
+                .await?;
+
+            Executor::<BoxFuture<'static, ()>>::execute(
+                &self.endpoint.executor,
+                Box::pin(async move {
+                    if let Err(e) = conn.await {
+                        tracing::debug!("connection task error: {:?}", e);
+                    }
+                }) as _,
+            );
+
+            Ok(SendRequest::from(send_request))
+        })
     }
 }
